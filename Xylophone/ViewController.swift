5956e34d87ec09a5e52d507837805609b75f6d67
Fabio <soaresfabiojr@gmail.com>
2020-05-23 16:13:26 +1200

    add function to play sound according to button pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -16,13 +16,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        
+        playSound(soundName: sender.currentTitle!)
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
